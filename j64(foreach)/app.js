cdc0ee36d551c866bdbe5b4f328f89a6599f8990
sajjad kafshgar123 <sajjad.kafshgar123@gmail.com>
2024-09-02 08:58:01 +0430

    update j65t

---
+++ j64(foreach)/app.js
@@ -0,0 +1,22 @@
+var username = [
+  "all",
+  "taghi",
+  "naggh",
+  "jaghi"
+]
+
+username.forEach(function(name){
+  // console.log(name)
+})
+
+var number = [
+  1,
+  2,
+  3,
+  4,
+  "kiram to kos zanet"
+]
+
+number.forEach(function(num){
+  console.log(num)
+})
---
+++ j65t/app.js
@@ -0,0 +1,9 @@
+var users = [
+    { id: 1 , name: "sajjad" , family:"kafshgar" , age : 18 , email:"sajjad.kafshgar123@gmail.com" },
+    { id: 2 , name: "farid" , family:"kia" , age : 20 , email:"fff@gmail.com" },
+    { id: 3 , name: "amir" , family:"saadati" , age : 25 , email:"aaa@gmail.com" }
+]
+
+users.forEach(function(user){
+    console.log("name  : " + user.name +"\n" + "family : " + user.family)
+})
